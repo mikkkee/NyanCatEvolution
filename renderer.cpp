3c943b66df6f8037984001f915c0a483bfbaca53
mikkkee <boy.swordman@gmail.com>
2015-12-07 21:07:05 +0800

    Start implementing alpha composition blend strategy

--- renderer.cpp
+++ renderer.cpp
@@ -6,7 +6,7 @@ namespace renderer{
 // Renders image by using opaque polygons.
 void Render(const DnaCanvas& canvas, cv::Mat& plot)
 {
-	plot.setTo(cv::Scalar(0, 0, 0));  // Reset the image color.
+	plot.setTo(cv::Scalar(0, 0, 0, 255));  // Reset the image color.
 	for (auto & polygon : *canvas.polygons) {
 		RenderPolygon(polygon, plot, 1.0, 1.0);
 	};
@@ -15,24 +15,24 @@ void Render(const DnaCanvas& canvas, cv::Mat& plot)
 void Render(const DnaCanvas& canvas, cv::Mat& plot,
 	const double scale_x, const double scale_y)
 {
-	plot.setTo(cv::Scalar(0, 0, 0));  // Reset the image color.
+	plot.setTo(cv::Scalar(0, 0, 0, 255));  // Reset the image color.
 	for (auto & polygon : *canvas.polygons) {
 		RenderPolygon(polygon, plot, scale_x, scale_y);
 	};
 }
-
+// Renders image by using semi-transparent polygons.
 void Render(const DnaCanvas& canvas, cv::Mat& plot, cv::Mat& subplot)
 {
-	plot.setTo(cv::Scalar(0, 0, 0));  // Reset the image color.
+	plot.setTo(cv::Scalar(0, 0, 0, 255));  // Reset the image color.
 	for (auto & polygon : *canvas.polygons) {
 		RenderPolygon(polygon, plot, subplot, 1.0, 1.0);
 	};
 }
-
+// Renders image by using semi-transparent polygons with scale factors.
 void Render(const DnaCanvas& canvas, cv::Mat& plot, cv::Mat& subplot,
 	const double scale_x, const double scale_y)
 {
-	plot.setTo(cv::Scalar(0, 0, 0));  // Reset the image color.
+	plot.setTo(cv::Scalar(0, 0, 0, 255));  // Reset the image color.
 	for (auto & polygon : *canvas.polygons) {
 		RenderPolygon(polygon, plot, subplot, scale_x, scale_y);
 	};
@@ -49,28 +49,64 @@ void RenderPolygon(const DnaPolygon& polygon, cv::Mat& plot,
 	cv::fillConvexPoly(plot, points, n, brush, 16);
 	delete points_vec;  // Releases vector allocated by new.
 }
-
+// Renders semi-transparent polygon by alpha composition blending method.
 void RenderPolygon(const DnaPolygon& polygon, cv::Mat& plot, cv::Mat& subplot,
 	const double scale_x, const double scale_y)
 {
-	subplot.setTo(cv::Scalar(0, 0, 0));
+	subplot.setTo(cv::Scalar(0, 0, 0, 0));
 	cv::Scalar brush = GetPolygonBrush(polygon);
 	std::vector<cv::Point>* points_vec = GetPolygonPointsVector(polygon, scale_x, scale_y);
 	cv::Point* points = &((*points_vec)[0]);
 	int n = static_cast<int>(polygon.points->size());
 	cv::fillConvexPoly(subplot, points, n, brush, 16);  // Plot polygon.
-	cv::add(plot, subplot, plot);                       // Blend polygon with canvas.
+
+	uchar* subplot_data = subplot.data;
+	uchar* plot_data = plot.data;
+	for (int y = 0; y < settings::MaxHeight; ++y) {
+		for (int x = 0; x < settings::MaxWidth; ++x) {
+			int subplot_blue = static_cast<int>(*subplot_data++);
+			int subplot_green = static_cast<int>(*subplot_data++);
+			int subplot_red = static_cast<int>(*subplot_data++);
+			int subplot_alpha = static_cast<int>(*subplot_data++);
+
+			int plot_blue = static_cast<int>(*plot_data++);
+			int plot_green = static_cast<int>(*plot_data++);
+			int plot_red = static_cast<int>(*plot_data++);
+			int plot_alpha = static_cast<int>(*plot_data++);
+
+			double alpha_fore = static_cast<double>(subplot_alpha) / 255.0;
+			double alpha_back = static_cast<double>(plot_alpha) / 255.0;
+			double alpha_multi = alpha_fore * alpha_back;
+			double alpha_tot = alpha_fore + alpha_back - alpha_multi;
+			double coeff_fore = alpha_fore / alpha_tot;
+			double coeff_back = (alpha_back - alpha_multi) / alpha_tot;
+
+			uchar r = static_cast<uchar>(coeff_fore * subplot_red + coeff_back * plot_red);
+			uchar g = static_cast<uchar>(coeff_fore * subplot_green + coeff_back * plot_green);
+			uchar b = static_cast<uchar>(coeff_fore * subplot_blue + coeff_back * plot_blue);
+			uchar a = static_cast<uchar>(coeff_fore * subplot_alpha + coeff_back * plot_alpha);
+
+			*(--plot_data) = a;
+			*(--plot_data) = r;
+			*(--plot_data) = g;
+			*(--plot_data) = b;
+
+			plot_data += 4;
+		};
+	};
+
+	// cv::add(plot, subplot, plot);                       // Blend polygon with canvas.
 	delete points_vec;                                  // Releases vector allocated by new.
 }
 
 cv::Scalar GetPolygonBrush(
 	const DnaPolygon& polygon)
 {
-	double alpha = static_cast<double>(polygon.brush->alpha) / 255;
-	int red = polygon.brush->red * alpha;
-	int green = polygon.brush->green * alpha;
-	int blue = polygon.brush->blue * alpha;
-	return cv::Scalar(blue, green, red, 255);
+	int alpha = polygon.brush->alpha;
+	int red = polygon.brush->red;
+	int green = polygon.brush->green;
+	int blue = polygon.brush->blue;
+	return cv::Scalar(blue, green, red, alpha);
 }
 
 std::vector<cv::Point>* GetPolygonPointsVector(
--- settings.h
+++ settings.h
@@ -14,15 +14,15 @@ const std::string DumpExtension = ".png";
 const int FitConvergence = 1000;
 
 // Rendering settings.
-const auto CanvasMatDataType = CV_8UC3;
-const int MaxWidth = 200;   // Sets the size used for fitness calculation.
-const int MaxHeight = 200;  // Adjust MaxHeight and MaxWidth to comprimise between speed and image quality.
+const auto CanvasMatDataType = CV_8UC4;
+const int MaxWidth = 314;   // Sets the size used for fitness calculation.
+const int MaxHeight = 314;  // Adjust MaxHeight and MaxWidth to comprimise between speed and image quality.
 
 // Mutation rates, a muration rate N means this mutation
 // happens with a probability of 1 / N.
 const int CanvasAddPolygonRate = 20;
-const int CanvasRemovePolygonRate = 100;
-const int CanvasMovePolygonRate = 100;
+const int CanvasRemovePolygonRate = 200;
+const int CanvasMovePolygonRate = 200;
 const int PointMaxMutationRate = 2000;  // Move anywhere in canvas.
 const int PointMidMutationRate = 600;   // Move around original position.
 const int PointMinMutationRate = 600;   // Move around original position slightly.
@@ -43,16 +43,16 @@ const int PolygonsPerCanvasMax = 250;
 const int PolygonsPerCanvasMin = 1;
 
 // Mutation range.
-const int ColorMutationRange = 5;
-const int PointMidMutationRange = 5;
-const int PointMinMutationRange = 5;
+const int ColorMutationRange = 15;
+const int PointMidMutationRange = 2;
+const int PointMinMutationRange = 2;
 const int BrushBlueMutationLow = 0;
 const int BrushBlueMutationHigh = 255;
 const int BrushGreenMutationLow = 0;
 const int BrushGreenMutationHigh = 255;
 const int BrushRedMutationLow = 0;
 const int BrushRedMutationHigh = 255;
-const int BrushAlphaMutationLow = 255;
-const int BrushAlphaMutationHigh = 255;
+const int BrushAlphaMutationLow = 10;
+const int BrushAlphaMutationHigh = 60;
 };
 #endif
